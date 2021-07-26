1d190077d27b40007d585df4f57f9892d0826b3a
Pete Lewis <mygit@pjblewis.com>
2021-07-26 09:12:43 -0700

    Notes on how to fix various things after waking

--- on-sleep.sh
+++ on-sleep.sh
@@ -9,7 +9,15 @@ if [ "$1" != "post" ] && [ "$1" != "resume" ]; then
   exit 0
 fi
 
-exec 1>/tmp/on-sleep.log >2&1
+exec 1>/tmp/on-sleep.log 2>&1
+
+# Reset monitor configuration
+# primary_monitor=...
+# secondary_monitor=...
+# xrandr --output "$primary_monitor" --auto --preferred --pos 0x0 --panning 0x0 --screen 0 --size 2560x1440 --output "$secondary_monitor" --auto --preferred --right-of "$primary_monitor"
+
+# Reset audio device if needed (use pactl list to figure this out)
+# pactl set-default-source alsa_output.pci-0000_04_00.1.hdmi-surround-extra1.monitor
 
 # Restart chrome GPU processes after PC wakes up
 /home/pete/setup-scripts/tools/restore-chrome.sh
