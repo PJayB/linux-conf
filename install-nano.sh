5b4c1fe79276d8e3870478bca580f6edd9fc3c99
Pete Lewis <mygit@pjblewis.com>
2021-10-24 22:42:13 -0700

    Script to get latest nano

---
+++ install-nano.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+nanodir="$HOME/nano"
+
+if [ ! -d "$nanodir" ]; then
+    git clone https://git.savannah.gnu.org/git/nano.git "$nanodir"
+fi
+
+sudo apt install -y\
+    autoconf \
+    automake \
+    autopoint \
+    gcc \
+    gettext \
+    git \
+    groff \
+    make \
+    pkg-config \
+    texinfo \
+    libncurses-dev \
+    ;
+
+cd "$nanodir"
+./autogen.sh
+./configure --enable-utf8
+make
+
+sudo apt purge -y nano || :
+sudo make install
+
+sudo update-alternatives --install \
+    /usr/bin/editor \
+    editor \
+    /usr/local/bin/nano \
+    30
+
+sudo update-alternatives --set \
+    editor \
+    /usr/local/bin/nano
