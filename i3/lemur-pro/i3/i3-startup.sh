d46a89a64e2f5285a1335b4881ee66b8c05b8c2e
Pete Lewis <mygit@pjblewis.com>
2022-06-10 21:27:52 -0700

    Restore monitor layout *before* nitrogen

--- i3/lemur-pro/i3/i3-startup.sh
+++ i3/lemur-pro/i3/i3-startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Set monitor layout
+if xrandr --properties | grep -qE '^DP-1 connected'; then
+    "$HOME/.screenlayout/acer-32-left-of-laptop.sh"
+fi
+
 # Start compositor
 if which picom && ! pgrep -x picom; then
     picom --experimental-backends --config ~/.config/i3/picom.conf -b
@@ -12,11 +17,6 @@ fi
 
 # Start pulseaudio
 start-pulseaudio-x11
-
-# Set window layout
-if xrandr --properties | grep -qE '^DP-1 connected'; then
-    "$HOME/.screenlayout/acer-32-left-of-laptop.sh"
-fi
 
 # Start a terminal
 i3-msg "workspace 1; layout stacking; exec i3-sensible-terminal;"
