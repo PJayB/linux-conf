6bc10fea380d56dab3a148e66f10de47ff78ab6e
Pete Lewis <mygit@pjblewis.com>
2022-06-12 15:36:02 -0700

    Commit new defaults to template i3 config

--- i3/i3-startup.sh
+++ i3/i3-startup.sh
@@ -7,31 +7,8 @@ if [ -f ~/.config/i3/custom-dpi ]; then
     xrandr --dpi $DPI
 fi
 
-# Start compositor
-if which picom && ! pgrep -x picom; then
-    picom --experimental-backends --config ~/.config/i3/picom.conf -b
-fi
-
-# Reset wallpaper
-if which nitrogen; then
-    nitrogen --restore
-fi
-
-# Start pulseaudio
-start-pulseaudio-x11
-
-# Start bluetooth tray
-if which blueman-applet; then
-    blueman-applet &
-fi
-
 # Start a terminal
-i3-msg "workspace 1; layout stacking; exec i3-sensible-terminal;"
-#if xrandr --listactivemonitors | grep -qE '^ 1:' ; then
-#    i3-msg "workspace 2; layout stacking; exec google-chrome"
-#else
-#    i3-msg "exec google-chrome"
-#fi
+i3-msg "workspace 1; exec i3-sensible-terminal;"
 
 # Execute custom scripts
 CUSTOMSCRIPT=~/.config/i3/custom-startup.sh
