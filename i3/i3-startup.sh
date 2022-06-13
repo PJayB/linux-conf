6e0fb3cb25a82e0ef43b5c8ed09de80281715665
Pete Lewis <mygit@pjblewis.com>
2022-06-13 12:43:34 -0700

    Startup improvements

--- i3/i3-startup.sh
+++ i3/i3-startup.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
+log_tag="i3-autostart"
 
-# Set the DPI of the display
-if [ -f ~/.config/i3/custom-dpi ]; then
-    DPI=$(cat ~/.config/i3/custom-dpi)
-    echo "Setting DPI to $DPI"
-    xrandr --dpi $DPI
-fi
+die() {
+    # print to syslog & stderr
+    logger -t "$log_tag" -s "$*"
 
-# Start a terminal
-i3-msg "workspace 1; exec i3-sensible-terminal;"
+    # abort
+    exit 1
+}
 
-# Execute custom scripts
-CUSTOMSCRIPT=~/.config/i3/custom-startup.sh
-if [ -f $CUSTOMSCRIPT ]; then
-    /bin/bash "$CUSTOMSCRIPT"
-fi
+logdir="$HOME/.config/i3-autostart-logs"
+mkdir -p "$logdir" || die "Failed to create $logdir"
+
+pids=( )
+
+run_detached() {
+    name="$(basename "$1")"
+    [ -n "$name" ] || die "Couldn't detect base name of '$1'"
+    logger -t "$log_tag" "Starting: $*"
+    nohup "$@" </dev/null >"$logdir/$name.log" 2>&1 &
+    pids+=( $! )
+}
+
+dunst_config_dir="$HOME/.config/dunst"
+i3_config_dir="$HOME/.config/i3"
+
+# Hand off suspend to lock screen
+run_detached xss-lock --transfer-sleep-lock -- "$i3_config_dir/lock.sh"
+
+# Start compositor
+run_detached picom --experimental-backends --config "$i3_config_dir/picom.conf" -b
+
+# Restore wallpaper
+run_detached variety --resume
+
+# Start pulse audio
+run_detached start-pulseaudio-x11
+
+# Start bluetooth applet
+run_detached blueman-applet
+
+# Start network applet
+run_detached nm-applet
+
+# Start dunst
+run_detached dunst -config "$dunst_config_dir/dunstrc"
 
--- i3/lemur-pro/i3/i3-startup.sh
+++ i3/lemur-pro/i3/i3-startup.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
+log_tag="i3-autostart"
+
+die() {
+    # print to syslog & stderr
+    logger -t "$log_tag" -s "$*"
+
+    # abort
+    exit 1
+}
+
+logdir="$HOME/.config/i3-autostart-logs"
+mkdir -p "$logdir" || die "Failed to create $logdir"
+
+pids=( )
+
+run_detached() {
+    name="$(basename "$1")"
+    [ -n "$name" ] || die "Couldn't detect base name of '$1'"
+    logger -t "$log_tag" "Starting: $*"
+    nohup "$@" </dev/null >"$logdir/$name.log" 2>&1 &
+    pids+=( $! )
+}
 
 # Set monitor layout
-if xrandr --properties | grep -qE '^DP-[[:number:]] connected'; then
+if xrandr --properties | grep -qE '^DP-[[:digit:]] connected'; then
     "$HOME/.screenlayout/acer-32-left-of-laptop.sh"
 fi
+
+dunst_config_dir="$HOME/.config/dunst"
+i3_config_dir="$HOME/.config/i3"
+
+# Hand off suspend to lock screen
+run_detached xss-lock --transfer-sleep-lock -- "$i3_config_dir/lock.sh"
+
+# Start compositor
+run_detached picom --experimental-backends --config "$i3_config_dir/picom.conf" -b
+
+# Restore wallpaper
+run_detached variety --resume
+
+# Start pulse audio
+run_detached start-pulseaudio-x11
+
+# Start bluetooth applet
+run_detached blueman-applet
+
+# Start network applet
+run_detached nm-applet
+
+# Start dunst
+run_detached dunst -config "$dunst_config_dir/dunstrc"
 
